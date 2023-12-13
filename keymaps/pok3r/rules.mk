a869d1cecba982fd287e87319350f00bfca1e9b8
Liāu Kiong-Gē <ddg01nb@duck.com>
2023-12-13 00:59:04 -0600

    move common to lkg_common

--- keymaps/pok3r/rules.mk
+++ keymaps/pok3r/rules.mk
@@ -4,7 +4,7 @@ TAP_DANCE_ENABLE = yes
 COMBO_ENABLE = yes
 NKRO_ENABLE = yes
 
-VPATH += keyboards/converter/adafruit_rp2040_usbh/keymaps/common
+VPATH += keyboards/converter/adafruit_rp2040_usbh/keymaps/lkg_common
 
-SRC += ../common/lkg_tap_dances.c
-SRC += ../common/lkg_key_process.c
+SRC += ../lkg_common/lkg_tap_dances.c
+SRC += ../lkg_common/lkg_key_process.c
