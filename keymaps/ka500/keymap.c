f684cadbbb5ff06630ddcf299f06102f76be28db
Liāu Kiong-Gē <ddg01nb@duck.com>
2023-12-19 20:51:12 -0600

    more OSM/OSL twaeks

--- keymaps/ka500/keymap.c
+++ keymaps/ka500/keymap.c
@@ -24,8 +24,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   
    Keymap: Default Layer in DVORAK
   
-   ,---------------------------------------------------------------------------------------------------.                    ,--------------------.
-   | DEL    |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F8  |  F9  |  F10 |  F12 | PSCR | SLCK | PAUS |		    | LGUI | RGUI | MENU |
+   ,-------------------------------------------------------------------------------------------------------------------.    ,--------------------.
+   |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | PSCR | SLCK |   PAUS |    | LGUI | RGUI | MENU |
    |--------+------+------+------+------+------+---------------------------+------+------+------+------+------+--------.    |------+------+------+------.
    | ESC    |  1!  |  2@  |  3#  |  4$  |  5%  |                           |  6^  |  7&  |  8   |  9(  |  0)  | \|     |    |  NL  |      |  /   |  *   | 
    |--------+------+------+------+------+------|                           +------+------+------+------+------+--------|    |------+------+------+------|
@@ -38,12 +38,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             | L2`~ | GU/M | Left | Right|                                         | Up   | Down | GU/[ | L2-]}|		                  |   .  | 
             `---------------------------'                                         `---------------------------'		                  `------' 
                                           ,-------------.         ,--------------.
-                                          | Ctrl | Alt  |         | LA(RA)| Ctrl |
+                                          | TD16 | TD18 |         | TD19 | TD17 |
                                    ,------|------|------|         |-------+------+------.  ,------.
                                    |      |      | L1PU |         | L1PU  |      |      |  |      |
                                    |Space | BSPC |------|         |-------| ENTR | Space|  |  0   |
-                                   |      |      |L3PU  |         | L3PU  |      |      |  |      |
+                                   |      |      | L3PU |         | L3PU  |      |      |  |      |
                                    `--------------------'         `---------------------'  `------' 
+
+   TD16: tap: OSM(LCTL);  held: LCTL;  tap-then-hold: Mo(_NAVI)
+   TD16: tap: OSM(RCTL);  held: RCTL;  tap-then-hold: Mo(_NAVI)
+   TD16: tap: OSM(LALT);  held: LALT;  tap-then-hold: Mo(_NAVI)
+   TD16: tap: RALT;       held: LALT;  tap-then-hold: Mo(_NAVI)
+
+
  */
 
 [_DVORAK] = LAYOUT_ka500(
@@ -82,19 +89,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		   
 		                                   KC_P1,              KC_P2,              KC_P3,                KC_PENT,             
 		   
-			 LT(_NAVI, KC_GRV),      LGUI_T(KC_APP),     KC_LEFT,            KC_RGHT,
+			 LT(_NAVI, KC_GRV),        LGUI_T(KC_APP),     KC_LEFT,            KC_RGHT,
 			 KC_UP,                    KC_DOWN,            RGUI_T(KC_LBRC),    LT(_NAVI, KC_RBRC),
 
                                                    KC_PDOT,
 		   
-			 KC_LCTL,                  KC_LALT,
-			 LALT_T(KC_RALT),          KC_RCTL,
+			 TD(DANCE_16),             TD(DANCE_18),
+			 TD(DANCE_19),             TD(DANCE_17),
 		   
-			 LT(_NAVI, KC_HOME),     LT(_EMACS_0, KC_END),
+			 LT(_NAVI, KC_HOME),       LT(_EMACS_0, KC_END),
 		   
 			 KC_SPC,                   KC_BSPC,
 
-			 LT(_NAVI, KC_PGUP),     LT(_EMACS_0, KC_PGDN),
+			 LT(_NAVI, KC_PGUP),       LT(_EMACS_0, KC_PGDN),
 
 			 KC_ENTER,                 KC_SPC,
 
@@ -105,16 +112,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   
    Keymap: layer 1 - Navigation keys 0 
   
-   ,---------------------------------------------------------------------------------------------------.                     ,--------------------.        
-   |        |      |      |      |      |      |      |      |      |      |      |      |      |      |		     |      |      |      |        
+   ,-------------------------------------------------------------------------------------------------------------------.     ,--------------------.        
+   |   L0   |  F13 |  F14 |  F15 | F16  | F17  | F18  | F19  |  F20 | F21  |  F22 |  F23 |  F24 |      |      |	 NKRO  |     |      |      |      |  
    |--------+------+------+------+------+------+---------------------------+------+------+------+------+------+--------.     +------+------+------+------.
-   |    F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                           |  F7  |  F8  |  F9  |  F10 | F11  |  F12   |     |      |      |      |      | 
+   |   TD4  | TD5  |  TD6 |  TD7 |  TD8 |  TD9 |                           | TD10 | TD11 | TD12 | TD13 | TD14 |  TD15  |     |      |      |      |      | 
    |--------+------+------+------+------+------|                           +------+------+------+------+------+--------|     +------+------+------+------+
-   |        |      | MB2  | MCU  | MB1  | MWU  |                           |      |      |  U   |      |      |        |     |      |      |      |      | 
+   |        |  V+  |  TD2 | MCU  | MB1  | MWU  |                           |      |      |  U   |      |      |        |     |      |      |      |      | 
    |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|     +------+------+------+------+
-   |        | MWL  | MCL  | MB3  | MCR  | MWR  |                           |      |  L   |      |  R   |      |        |     |      |      |      |      | 
+   |        | MWL  | MCL  | MCd  | MCR  | MWR  |                           | MB1  |  L   |  D   |  R   |      |        |     |      |      |      |      | 
    |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|     +------+------+------+------+
-   |        |      | BBW  | MCD  | BFW  | MWD  |                           | MB1  |      |  D   |      |      |        |     |      |      |      |      | 
+   |        | TD3  | BBW  | WSC  | BFW  | MWD  |                           |      |      |      |      |      |        |     |      |      |      |      | 
    `--------+------+------+------+------+-------                           `------+------+------+------+------+--------'     +------+------+------+------'
             |      |      |      |      |                                         |      |      |      |      |		                   |      |        
             `---------------------------'                                         `---------------------------'		                   `------'        
@@ -125,62 +132,80 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    |      |      |------|         |-------|      |      |  |      |
                                    |      |      |      |         |       |      |      |  |      |
                                    `--------------------'         `---------------------'  `------'
+
+
+        L0:   tap to _DVORAK
+        TD2:  tap: mouse button 2, held: mouse button 3
+	TD3:  tap: volume down, held mute 
+        TD4:  tap: F1,  held: LCTL + F1,  double-tap: LALT + F1,  tap-then-held: LCTL+ALT+F1
+        TD5:  tap: F2,  held: LCTL + F2,  double-tap: LALT + F2,  tap-then-held: LCTL+ALT+F2
+        TD6:  tap: F3,   eld: LCTL + F1,  double-tap: LALT + F3,  tap-then-held: LCTL+ALT+F1
+        TD7:  tap: F4,  held: LCTL + F1,  double-tap: LALT + F4,  tap-then-held: LCTL+ALT+F4
+        TD8:  tap: F5,  held: LCTL + F1,  double-tap: LALT + F5,  tap-then-held: LCTL+ALT+F5
+        TD9:  tap: F6,  held: LCTL + F6,  double-tap: LALT + F16, tap-then-held: LCTL+ALT+F6
+        TD10: tap: F7,  held: LCTL + F7,  double-tap: LALT + F7,  tap-then-held: LCTL+ALT+F7
+        TD11: tap: F8,  held: LCTL + F8,  double-tap: LALT + F18, tap-then-held: LCTL+ALT+F8
+        TD12: tap: F9,  held: LCTL + F9,  double-tap: LALT + F9,  tap-then-held: LCTL+ALT+F9
+        TD13: tap: F10, held: LCTL + F10, double-tap: LALT + F10, tap-then-held: LCTL+ALT+F10
+        TD14: tap: F11, held: LCTL + F11, double-tap: LALT + F11, tap-then-held: LCTL+ALT+F11
+        TD15: tap: F12, held: LCTL + F12, double-tap: LALT + F12, tap-then-held: LCTL+ALT+F12
+
  */
 
 [_NAVI] = LAYOUT_ka500(
-		   QK_BOOT,
-		   NK_ON,               KC_NO,              KC_NO,            KC_NO,            KC_NO,           KC_NO,
-		   KC_NO,               KC_NO,              KC_NO,            KC_NO,            KC_NO,           KC_NO,
-		   KC_NO,               KC_NO,              KC_NO,
+		       TO(_DVORAK),
+		       KC_F13,              KC_F14,             KC_F15,           KC_F16,           KC_F17,          KC_F18,
+		       KC_F19,              KC_F20,             KC_F21,           KC_F22,           KC_F23,          KC_F24,
+		       KC_NO,               KC_NO,              NK_ON,
 
-		   KC_NO,               KC_NO,              KC_NO,
+		       KC_NO,               KC_NO,              KC_NO,
 		   
-		   KC_F1,
-		   KC_F2,               KC_F3,              KC_F4,            KC_F5,            KC_F6,
-		   KC_F7,               KC_F8,              KC_F9,            KC_F10,           KC_F11,
-		   KC_F12,
+		       TD(DANCE_4),
+		       TD(DANCE_5),         TD(DANCE_6),        TD(DANCE_7),      TD(DANCE_8),      TD(DANCE_9),
+		       TD(DANCE_10),        TD(DANCE_11),       TD(DANCE_12),     TD(DANCE_13),     TD(DANCE_14),
+		       TD(DANCE_15),
 		   
-		                        KC_NO,                                KC_NO,            KC_NO,     
+		       KC_NO,                                   KC_NO,            KC_NO,     
 		   
-		   KC_NO,
-		   KC_NO,               KC_MS_BTN2,         KC_MS_U,          KC_MS_BTN1,       KC_WH_U,
-		   KC_NO,               KC_NO,              KC_UP,            KC_NO,            KC_NO,
-		   KC_NO,
+		       KC_NO,
+		       KC_VOLU,             TD(DANCE_2),        KC_MS_U,          KC_MS_BTN1,       KC_WH_U,
+		       KC_NO,               KC_NO,              KC_UP,            KC_NO,            KC_NO,
+		       KC_NO,
 		   
-		                        KC_NO,              KC_NO,            KC_NO,            KC_NO,             
+		       KC_NO,               KC_NO,              KC_NO,            KC_NO,             
 		   
-		   KC_NO,
-		   KC_WH_L,             KC_MS_L,            KC_MS_BTN3,       KC_MS_R,          KC_WH_R,
-		   KC_NO,               KC_LEFT,            KC_NO,            KC_RIGHT,         KC_NO,
-		   KC_NO,
+		       KC_NO,
+		       KC_WH_L,             KC_MS_L,            KC_MS_D,          KC_MS_R,          KC_WH_R,
+		       KC_NO,               KC_LEFT,            KC_DOWN,          KC_RIGHT,         KC_NO,
+		       KC_NO,
 		   
-		                        KC_NO,              KC_NO,            KC_NO,            KC_NO,             
+		       KC_NO,               KC_NO,              KC_NO,            KC_NO,             
 
-		   KC_NO,
-		   KC_NO,               KC_WBAK,            KC_MS_D,          KC_WFWD,          KC_WH_D,
-		   KC_NO,               KC_NO,              KC_NO,            KC_NO,            KC_NO,
-		   KC_NO,
+		       KC_NO,
+		       TD(DANCE_3),         KC_WBAK,            KC_WSCH,          KC_WFWD,          KC_WH_D,
+		       KC_NO,               KC_NO,              KC_NO,            KC_NO,            KC_NO,
+		       KC_NO,
 		   		   
-		                        KC_NO,              KC_NO,            KC_NO,            KC_NO,             
+		       KC_NO,               KC_NO,            KC_NO,            KC_NO,             
 
-		   KC_NO,               KC_NO,              KC_NO,            KC_NO,
-		   KC_NO,               KC_NO,              KC_NO,            KC_NO,
+		       KC_NO,               KC_NO,              KC_NO,            KC_NO,
+		       KC_NO,               KC_NO,              KC_NO,            KC_NO,
 
-                                                            KC_NO,
+                       KC_NO,
 		   
-		   KC_NO,               KC_NO,
-		   KC_NO,               KC_NO,
+		       KC_NO,               KC_NO,
+		       KC_NO,               KC_NO,
 		   
-                   KC_NO,               KC_NO,
+		       KC_NO,               KC_NO,
 		   
-                   KC_NO,               KC_NO,
+		       KC_NO,               KC_NO,
 
-		   KC_NO,               KC_NO,
+		       KC_NO,               KC_NO,
 
-		   KC_NO,               KC_NO,
+		       KC_NO,               KC_NO,
 
-		                        KC_NO
-                   ),
+		       KC_NO
+		       ),
 
 /*
   
